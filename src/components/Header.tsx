90e199e5a07f5e84efca243d73a555ed5b1adfe9
William Bojczuk <williambojczuk@gmail.com>
2023-08-14 11:38:09 -0400

    initial commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,7 @@ export default function Header() {
   return (
     <header>
         <img id="mainLogo" src="./img/mainlogo.png"/>
-        <h1 id="inBuildingTitle">New Site In Building!</h1>
-        <div id="inBuildingAnim">
-            <Lottie
-            animationData={animationData}
-            play={true}
-            loop={true}
-            >
-            </Lottie>
-        </div>
+
     </header>
   )
 }
