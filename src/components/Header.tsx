77c0ec5980a30fda48d3f8fb8444d0bb0af90d94
William Bojczuk <williambojczuk@gmail.com>
2023-08-14 15:44:14 -0400

    added animation in src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import "./header.css"
-import animationData from "./in_building_anim.json"
-import Lottie from "react-lottie-player/dist/LottiePlayerLight"
+
 
 export default function Header() {
   return (
