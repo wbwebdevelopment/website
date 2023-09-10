378a2eee7376065dde29d458a1a288c7d7cf6f3c
William Bojczuk <williambojczuk@gmail.com>
2023-09-10 04:18:04 -0700

    Added new work

--- src/components/OurWork/projectData.ts
+++ src/components/OurWork/projectData.ts
@@ -6,6 +6,11 @@ interface projectType{
 
 const projectData: projectType[] = [
     {
+        title: "Cartersville Outreach Int.",
+        link: "https://cartersvilleoutreachinternational.org",
+        imgSrc: "https://i.imgur.com/Dlc8LYA.png"
+    },
+    {
         title: "Superior Pressure Washing LLC",
         link: "https://superiorwashing.net",
         imgSrc: "https://i.imgur.com/1AYL0v0.png"
