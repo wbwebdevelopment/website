2b051632b3529c984d16f302e30bfc790e92bd41
William Bojczuk <williambojczuk@gmail.com>
2023-09-11 11:43:51 -0400

    added leave review link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -38,9 +38,9 @@ export default function Header() {
     <header>
       <div id="logoWrapper">
         <img id="mainLogo" src="./img/mainlogo.png"/>
-        <a href="#contactFormTitle" id="getInTouch" className="get-in-touch-button">
+        <a href="/leavereview" target="_blank" id="getInTouch" className="get-in-touch-button">
           <img src="./img/icons/form-icon.svg" alt="form icon" />
-          <span>Get In Touch!</span>
+          <span>Leave a Review!</span>
           </a>
       </div>
 
