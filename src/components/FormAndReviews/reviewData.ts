8d4e0367e4d453a84aa7950b8ce1c230f6b45b7b
William Bojczuk <williambojczuk@gmail.com>
2023-09-24 11:03:08 -0700

    added review

--- src/components/FormAndReviews/reviewData.ts
+++ src/components/FormAndReviews/reviewData.ts
@@ -5,36 +5,14 @@ interface reviewDataTypes{
 
 const reviewData: reviewDataTypes[] = [
     {
-        name: "Johnny Appleseed",
-        desc: "Lorem sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        name: "David, COI",
+        desc: "Had William create a website for our international ministry. Did an amazing job with great detail. I definitely recommend using him. I will definitely use him again in the future.."
     },
     {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        name: "Nichelle, Fiverr",
+        desc: "What a fantastic find!!! I have added William to my list of Fiverr gig favorites. He went above and beyond. I am proud to present the website to the public now. He pays attention to the little details! I appreciate the extra care very much!"
     },
-    {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },{
-        name: "Johnny Appleseed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
+    
 ]
 
 export default reviewData
