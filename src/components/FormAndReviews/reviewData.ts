d70712acd886abea86136fd06cad73b3555962fe
William Bojczuk <williambojczuk@gmail.com>
2023-10-01 22:41:00 -0400

    added some reviews

--- src/components/FormAndReviews/reviewData.ts
+++ src/components/FormAndReviews/reviewData.ts
@@ -5,13 +5,20 @@ interface reviewDataTypes{
 
 const reviewData: reviewDataTypes[] = [
     {
-        name: "David, COI",
-        desc: "Had William create a website for our international ministry. Did an amazing job with great detail. I definitely recommend using him. I will definitely use him again in the future.."
+        name: "Alex - PushMusic, Fiverr",
+        desc: "Super happy I found William's page! I had some HTML that I needed revised to customize a search bar on my website. William was able to assist timely and efficiently. If/when I need someone to write or edit custom code, I will definitely turn to William again!"
     },
+    
     {
         name: "Nichelle, Fiverr",
         desc: "What a fantastic find!!! I have added William to my list of Fiverr gig favorites. He went above and beyond. I am proud to present the website to the public now. He pays attention to the little details! I appreciate the extra care very much!"
     },
+
+    {
+        name: "David, COI",
+        desc: "Had William create a website for our international ministry. Did an amazing job with great detail. I definitely recommend using him. I will definitely use him again in the future.."
+    },
+    
     
 ]
 
